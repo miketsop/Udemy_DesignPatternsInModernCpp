7ea65f1727032bc677697297518b5ba8e28d8582
Michael Tsopelas <miketsop@Michaels-MacBook-Air.local>
2020-12-15 13:50:48 +0100

    Added example of Array Backed Properties

---
+++ Composite/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <string>
+#include <array>
+#include <functional>
+
+using namespace std;
+
+class Creature {
+    enum Abilities {strength, agility, intelligence, count};
+    array<int, count> abilities;
+
+public:
+    int get_strength(){return abilities[strength];}
+    void set_strength(int val) {abilities[strength] = val;}
+
+    int get_agility(){return abilities[agility];}
+    void set_agility(int val) {abilities[agility] = val;}
+
+    int get_intelligence(){return abilities[intelligence];}
+    void set_intelligence(int val) {abilities[intelligence] = val;}
+
+    int sum() const
+    {
+        return accumulate(abilities.begin(), abilities.end(), 0);
+    }
+
+    double average() const 
+    {
+        return sum() / (double)count;
+    }
+
+    int max() const {
+        return *max_element(abilities.begin(), abilities.end());
+    }
+};
+
+int main()
+{
+    Creature orc;
+    orc.set_strength(16);
+    orc.set_agility(15);
+    orc.set_intelligence(5);
+
+    cout << "Orc has an average statistic of " << orc.average() << " and a max. of " << orc.max() << endl;
+    return 0;
+}
